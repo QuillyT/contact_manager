168bed255b03cc85d8a3af1739e6966ccd6b7373
QuillyT <QuillyT@gmail.com>
2013-11-25 13:27:05 -0700

    Add PhoneNumber validations

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe PhoneNumber do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) { PhoneNumber.new(number: "2021231234", person_id: 1)}
+
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+
+  it 'is invalid  without a number' do
+    phone_number.number = nil
+    expect(phone_number).to_not be_valid
+  end
+
+  it 'is associated with a person' do 
+    expect(phone_number).to respond_to(:person)
+  end
 end
