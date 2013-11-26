d0442197eba9eb80022397cf3ea9189252981d18
QuillyT <QuillyT@gmail.com>
2013-11-26 00:15:11 -0700

    Add delete phone number feature. Finish implementing phone number functionality

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -151,6 +151,9 @@ describe PhoneNumbersController do
   end
 
   describe "DELETE destroy" do
+    let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+    let(:valid_attributes) { { number: '555-0123', person_id: bob.id } }
+
     it "destroys the requested phone_number" do
       phone_number = PhoneNumber.create! valid_attributes
       expect {
@@ -158,10 +161,10 @@ describe PhoneNumbersController do
       }.to change(PhoneNumber, :count).by(-1)
     end
 
-    it "redirects to the phone_numbers list" do
+    it "redirects to the phone number's person" do
       phone_number = PhoneNumber.create! valid_attributes
       delete :destroy, {:id => phone_number.to_param}, valid_session
-      response.should redirect_to(phone_numbers_url)
+      response.should redirect_to(phone_number.person)
     end
   end
 
